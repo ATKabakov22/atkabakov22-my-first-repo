faba9258bd5cdc5a6ede9e9fa0149b157c0cc088
Anton Kabakov <ATKabakov22@codingburgas.bg>
2023-05-10 14:36:41 +0300

    l

--- cpp.cpp
+++ cpp.cpp
@@ -1,23 +1,4 @@
-#pragma once
-
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-
-	
-		int n;
-		cin >> n;
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = i; j <= 2 * i - 1; j++)
-			{
-				cout << j << " ";
-			}
-			cout << endl;
-		}
-	
-}
-l
