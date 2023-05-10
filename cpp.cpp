ba92109cf989540e9365932d50f76694977dc680
Anton Kabakov <ATKabakov22@codingburgas.bg>
2023-05-10 16:00:26 +0300

    a

--- cpp.cpp
+++ cpp.cpp
@@ -2,3 +2,44 @@
 
 using namespace std;
 
+int main()
+{
+	float arr[29], aarr = 0, var = 0, arra = 1, a = 1;
+
+	for (int i = 0; i < 30; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < 30; i++)
+	{
+		aarr = aarr + arr[i];
+	}
+
+	cout << "Average = " << aarr / 30 << "\n";
+
+	for (int i = 10; i < 20; i++)
+	{
+		var = var + arr[i];
+
+	}
+
+	cout << "Average 11-20 = " << var/10 << "\n";
+
+	aarr=aarr/30;
+
+	for (int i = 0; i < 30; i++)
+	{
+		if (arra < aarr)
+		{
+			cout << arra << " ";
+			arra++;
+		}
+
+		if (arr[i]<aarr)
+		{
+			a++;
+		}
+	}
+	cout << a;
+}	
